994e444a7ae013fc22dc8f571292383b80ed886a
niceusts <niceusantos1365@gmail.com>
2024-09-25 12:26:04 -0300

    issue: add section de contato e habilidades

---
+++ src/app/components/ContactSection.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import Image from 'next/image';
+import { Facebook, Github } from 'lucide-react';
+
+const ContactSection = () => {
+  return (
+    <section className="py-12">
+      <div className="container mx-auto flex flex-col lg:flex-row items-center justify-between">
+        <div className="w-full lg:w-1/2 text-white flex flex-col items-center lg:items-start mb-8 lg:mb-0">
+          <div className="mb-6 w-96">
+            <Image
+              src="/Contact.png"
+              alt="Ilustração"
+              layout="responsive"
+              width={50}
+              height={50}
+            />
+          </div>
+
+          {/* Links para LinkedIn e GitHub */}
+          <div className="flex space-x-4 text-white">
+            <a
+              href="https://www.linkedin.com/in/niceu-santos-biriba-1055301b2/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center space-x-2"
+            >
+              <Facebook size={24} />
+              <span>LinkedIn</span>
+            </a>
+            <a
+              href="https://github.com/niceusts"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center space-x-2"
+            >
+              <Github size={24} />
+              <span>GitHub</span>
+            </a>
+          </div>
+        </div>
+
+        {/* Formulário de Contato */}
+        <div className="w-full lg:w-1/2 bg-gray-900 p-8 rounded-lg text-white">
+          <h3 className="text-lg font-semibold mb-6 text-center">
+            Deixe seu recado.
+          </h3>
+
+          <form>
+            <div className="mb-4">
+              <label htmlFor="name" className="block text-sm mb-1">
+                Nome:
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="w-full p-2 border border-gray-600 rounded bg-black text-white"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-sm mb-1">
+                Email:
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="w-full p-2 border border-gray-600 rounded bg-black text-white"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="phone" className="block text-sm mb-1">
+                Telefone:
+              </label>
+              <input
+                type="text"
+                id="phone"
+                className="w-full p-2 border border-gray-600 rounded bg-black text-white"
+                placeholder="opcional"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="message" className="block text-sm mb-1">
+                Mensagem:
+              </label>
+              <textarea
+                id="message"
+                rows={4}
+                className="w-full p-2 border border-gray-600 rounded bg-black text-white"
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-purple-600 hover:bg-purple-800 text-white py-2 px-4 rounded"
+            >
+              Enviar
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ContactSection;
--- src/app/components/HabilitySection.tsx
+++ src/app/components/HabilitySection.tsx
@@ -2,12 +2,83 @@ import React from 'react';
 
 const HabilitySection = () => {
   return (
-    <section>
+    <section className="py-12">
       <div className="container mx-auto">
-        <h3 className="text-2xl font-bold text-center mt-16 mb-8">
+        <h3 className="text-2xl font-bold text-center  mt-16 mb-8">
           <span className="text-blue-500">[</span> Habilidades{' '}
           <span className="text-blue-500">]</span>
         </h3>
+
+        {/* Grid de Habilidades */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 px-5">
+          {/* Habilidade 1 - HTML */}
+          <div className="bg-gray-900 text-white p-4 rounded-lg">
+            <h4 className="text-lg font-bold text-orange-500">HTML</h4>
+            <p className="text-sm mt-2">Nível de conhecimento: 90%</p>
+            <div className="relative pt-1 mt-2">
+              <div className="overflow-hidden h-2 text-xs flex rounded bg-blue-900">
+                <div
+                  style={{ width: '85%' }}
+                  className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-orange-500"
+                ></div>
+              </div>
+            </div>
+            <button className="mt-4 bg-blue-600 hover:bg-blue-900 text-white py-2 px-4 rounded w-full">
+              Ver mais
+            </button>
+          </div>
+
+          {/* Habilidade 2 - CSS */}
+          <div className="bg-gray-900 text-white p-4 rounded-lg">
+            <h4 className="text-lg font-bold text-blue-500">CSS</h4>
+            <p className="text-sm mt-2">Nível de conhecimento: 70%</p>
+            <div className="relative pt-1 mt-2">
+              <div className="overflow-hidden h-2 text-xs flex rounded bg-blue-900">
+                <div
+                  style={{ width: '70%' }}
+                  className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"
+                ></div>
+              </div>
+            </div>
+            <button className="mt-4 bg-blue-600 hover:bg-blue-900 text-white py-2 px-4 rounded w-full">
+              Ver mais
+            </button>
+          </div>
+
+          {/* Habilidade 3 - JavaScript */}
+          <div className="bg-gray-900 text-white p-4 rounded-lg">
+            <h4 className="text-lg font-bold text-yellow-500">JAVASCRIPT</h4>
+            <p className="text-sm mt-2">Nível de conhecimento: 60%</p>
+            <div className="relative pt-1 mt-2">
+              <div className="overflow-hidden h-2 text-xs flex rounded bg-blue-900">
+                <div
+                  style={{ width: '60%' }}
+                  className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-yellow-500"
+                ></div>
+              </div>
+            </div>
+            <button className="mt-4 bg-blue-600 hover:bg-blue-900 text-white py-2 px-4 rounded w-full">
+              Ver mais
+            </button>
+          </div>
+
+          {/* Habilidade 4 - React */}
+          <div className="bg-gray-900 text-white p-4 rounded-lg">
+            <h4 className="text-lg font-bold text-red-400">REACT</h4>
+            <p className="text-sm mt-2">Nível de conhecimento: 50%</p>
+            <div className="relative pt-1 mt-2">
+              <div className="overflow-hidden h-2 text-xs flex rounded bg-blue-900">
+                <div
+                  style={{ width: '50%' }}
+                  className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-400"
+                ></div>
+              </div>
+            </div>
+            <button className="mt-4 bg-blue-600 hover:bg-blue-900 text-white py-2 px-4 rounded w-full">
+              Ver mais
+            </button>
+          </div>
+        </div>
       </div>
     </section>
   );
