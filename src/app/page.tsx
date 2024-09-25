71bf07be1193efc30dda468086b33a2d857c837d
niceusts <niceusantos1365@gmail.com>
2024-09-25 12:26:24 -0300

    add lucide-react

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import HeroSection from './components/HeroSection';
 import ProjectSection from './components/ProjectSection';
 import HabilitySection from './components/HabilitySection';
+import ContactSection from './components/ContactSection';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <HeroSection />
       <ProjectSection />
       <HabilitySection />
+      <ContactSection />
     </div>
   );
 }
