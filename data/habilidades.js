3d3ed1975936a3486456cd0756aaf7a16b51418c
niceusts <niceusantos1365@gmail.com>
2024-09-30 11:48:47 -0300

    feat: db de habilidades pege

---
+++ data/habilidades.js
@@ -0,0 +1,81 @@
+// src/data/habilidades.js
+export const habilidades = {
+  frontend: [
+    {
+      id: 1,
+      title: 'HTML',
+      knowledgeLevel: '85%',
+      color: 'orange-500',
+      percentage: '85%',
+      bgColor: 'bg-orange-500',
+      logo: '/javascript.png',
+    },
+    {
+      id: 2,
+      title: 'CSS',
+      knowledgeLevel: '70%',
+      color: 'blue-500',
+      percentage: '70%',
+      bgColor: 'bg-blue-500',
+      logo: '/javascript.png',
+    },
+    {
+      id: 3,
+      title: 'JavaScript',
+      knowledgeLevel: '70%',
+      color: 'yellow-500',
+      percentage: '70%',
+      bgColor: 'bg-yellow-500',
+      logo: '/javascript.png',
+    },
+    {
+      id: 4,
+      title: 'React',
+      knowledgeLevel: '50%',
+      color: 'red-400',
+      percentage: '50%',
+      bgColor: 'bg-red-400',
+      logo: '/javascript.png',
+    },
+  ],
+  backend: [
+    {
+      id: 5, // Alterado para um ID único
+      title: 'Node.js',
+      knowledgeLevel: '85%',
+      color: 'green-500', // Adicionando cor para consistência
+      percentage: '85%',
+      bgColor: 'bg-green-500',
+      logo: '/javascript.png',
+    },
+    {
+      id: 6, // Alterado para um ID único
+      title: 'Python',
+      knowledgeLevel: '75%',
+      color: 'blue-500', // Adicionando cor para consistência
+      percentage: '75%',
+      bgColor: 'bg-blue-500',
+      logo: '/javascript.png',
+    },
+  ],
+  database: [
+    {
+      id: 7, // Alterado para um ID único
+      title: 'MySQL',
+      knowledgeLevel: '80%',
+      color: 'purple-500', // Adicionando cor para consistência
+      percentage: '80%',
+      bgColor: 'bg-purple-500',
+      logo: '/javascript.png',
+    },
+    {
+      id: 8, // Alterado para um ID único
+      title: 'MongoDB',
+      knowledgeLevel: '70%',
+      color: 'green-400', // Adicionando cor para consistência
+      percentage: '70%',
+      bgColor: 'bg-green-400',
+      logo: '/javascript.png',
+    },
+  ],
+};
